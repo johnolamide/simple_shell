f173efe72efa7002636cc61eb6e684364b8a30b2
John Olamide Alokan <alokanolamide@gmail.com>
2023-05-22 07:28:04 -0700

    [FIX] simple_shell

--- simple_shell.c
+++ simple_shell.c
@@ -93,6 +93,7 @@ char **_tokenize(char *prompt, char *delim)
 void _execute(char *prompt, char *argv[], char *envp[])
 {
 	int status;
+	char *inputCommand = NULL;
 	pid_t pid = fork();
 
 	if (pid == 0)
@@ -102,8 +103,9 @@ void _execute(char *prompt, char *argv[], char *envp[])
 			_printenv();
 			exit(EXIT_SUCCESS);
 		}
-		execve(prompt, argv, envp);
-		perror(prompt);
+		inputCommand = get_path(prompt);
+		execve(inputCommand, argv, envp);
+		perror(inputCommand);
 		exit(EXIT_SUCCESS);
 	}
 	else
@@ -123,4 +125,55 @@ void _printenv(void)
 		write(STDOUT_FILENO, environ[i], strlen(environ[i]));
 		write(STDOUT_FILENO, "\n", 1);
 	}
+}
+
+/**
+ * get_path - gets or fetches the PATH of a file
+ * @command: the PATH to be found
+ * Return: an array of string.
+ */
+
+char *get_path(char *command)
+{
+	char *path, *path_copy;
+	int commandlength;
+	char *path_token;
+	int dir_length;
+	char *file_path;
+	struct stat buffer;
+
+	path = getenv("PATH");
+
+	if (path)
+	{
+		path_copy = strdup(path);
+		commandlength = strlen(command);
+		path_token = strtok(path_copy, ":");
+
+		while (path_token != NULL)
+		{
+			dir_length = strlen(path_token);
+			file_path = malloc(commandlength + dir_length + 2);
+			strcpy(file_path, path_token);
+			strcat(file_path, "/");
+			strcat(file_path, command);
+			strcat(file_path, "\0");
+
+			if (stat(file_path, &buffer) == 0)
+			{
+				free(path_copy);
+				return (file_path);
+			}
+			else
+			{
+				free(file_path);
+				path_token = strtok(NULL, ":");
+			}
+		}
+		free(path_copy);
+		if (stat(command, &buffer) == 0)
+			return (command);
+		return (NULL);
+	}
+	return (NULL);
 }
